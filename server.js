c96180dc415fe7572b3daf7eceeac547f2d2c206
Mario <mariolugo23@gmail.com>
2014-02-18 22:11:10 -0700

    Basic setup of express

---
+++ server.js
@@ -0,0 +1,23 @@
+var express	= require('express');
+
+var env = process.env.NODE_ENV || 'development';
+
+var app = express();
+
+app.configure(function(){
+	app.set('views', __dirname + '/server/views');
+	app.set('view engine', 'Jade');
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+
+	app.use(express.static(__dirname + '/public'));
+});
+
+app.get('*', function(req, res){
+	res.render('index');
+});
+
+var port = process.env.PORT || 3030;
+app.listen(port);
+
+console.log("Listening on port "+ port);
